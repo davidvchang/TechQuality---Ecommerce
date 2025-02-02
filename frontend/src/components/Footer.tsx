b36ed7b2968fc89bf6e56cbb7d6a19100ae05cd2
davidvchang <dvalenzuelachang@gmail.com>
2025-02-02 01:11:40 -0700

    Add: Footer finished

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Instagram, Facebook, MapPin, Phone, Mail } from 'lucide-react';
+import { Instagram, Facebook, MapPin, Phone, Mail, Twitter } from 'lucide-react';
 import IconFooter from './IconFooter';
 
 const Footer:React.FC = () => {
@@ -10,12 +10,35 @@ const Footer:React.FC = () => {
                 <span className='font-medium text-lg'>About Tech<span className='text-blue-500'>Quality</span></span>
                 <p className='text-slate-600 font-light'>We handle the best quality and the best prices, we offer high quality and long lasting products, we have a great variety of products that you will like.</p>
 
-                <div className='flex gap-3 items-center '>
+                <div className='flex gap-4 items-center '>
                     <IconFooter link='#' icon={<Facebook className='w-5 h-5 text-slate-400 hover:text-slate-600 hover:transition duration-300'/>}/>
                     <IconFooter link='#' icon={<Instagram className='w-5 h-5 text-slate-400 hover:text-slate-600 hover:transition duration-300'/>}/>
+                    <IconFooter link='#' icon={<Twitter className='w-5 h-5 text-slate-400 hover:text-slate-600 hover:transition duration-300'/>}/>
 
                 </div>
             </div>
+
+
+            <div className='flex flex-col max-w-72 gap-5'>
+                <span className='font-medium text-lg'>Links</span>
+
+                <div className='flex gap-2 items-center'>
+                    <a href='#' className='text-slate-600 font-light'>Products</a>
+                </div>
+
+                <div className='flex gap-2 items-center'>
+                    <a href='#' className='text-slate-600 font-light'>Categories</a>
+                </div>
+
+                <div className='flex gap-2'>
+                    <a href='#' className='text-slate-600 font-light'>About</a>
+                </div>
+
+                <div className='flex gap-2'>
+                    <a href='#' className='text-slate-600 font-light'>Login</a>
+                </div>
+            </div>
+
 
             <div className='flex flex-col max-w-72 gap-5'>
                 <span className='font-medium text-lg'>Contact Us</span>
