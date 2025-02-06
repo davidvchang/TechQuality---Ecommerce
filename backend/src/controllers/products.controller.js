d6f5ae58224158b426a33bb6dc44f3e02d237e04
davidvchang <dvalenzuelachang@gmail.com>
2025-02-06 00:22:14 -0700

    Add: Endpoint getProducts

--- backend/src/controllers/products.controller.js
+++ backend/src/controllers/products.controller.js
@@ -1,5 +1,10 @@
 import {pool} from '../bd.js'
 
-export const getProducts = async () => {
-
+export const getProducts = async (req, res) => {
+    try {
+        const products = await pool.query("SELECT * FROM products")
+        res.status(200).json(products.rows)
+    } catch (ex) {
+        res.status(500).json({message: "Ha ocurrido un error:" + ex})
+    }
 }
