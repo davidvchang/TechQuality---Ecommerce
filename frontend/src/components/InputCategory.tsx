dbfe940bb3063a0ced65adb92bf8da91eb2ddd2c
davidvchang <dvalenzuelachang@gmail.com>
2025-02-08 01:17:43 -0700

    Add: All lateral categories, prices and sort in ProductsPage

--- frontend/src/components/InputCategory.tsx
+++ frontend/src/components/InputCategory.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
 interface PropsCategories {
+    name_input: string,
     id_name: string,
     name: string
 }
 
-const InputCategory:React.FC<PropsCategories> = ({ id_name, name }) => {
+const InputCategory:React.FC<PropsCategories> = ({ name_input, id_name, name }) => {
   return (
     <div className='flex gap-3 items-center'>
-        <input type="radio" name="categories" id={id_name} className='w-4 h-4'/>
+        <input type="radio" name={name_input} id={id_name} className='w-4 h-4'/>
         <label htmlFor={id_name} className='font-light text-sm'>{name}</label>
       
     </div>
--- frontend/src/pages/Products.tsx
+++ frontend/src/pages/Products.tsx
@@ -44,8 +44,45 @@ const Products:React.FC = () => {
 
             <div className='flex flex-col gap-3'>
                 {uniqueCategories.map((c) => (
-                    <InputCategory id_name={c.name} key={c.id_product} name={c.category}/>
+                    <InputCategory name_input='categories' id_name={c.name} key={c.id_product} name={c.category}/>
                 ))}
+            </div>
+        </div>
+
+        <div className='pt-8 px-20 flex flex-col gap-4'>
+            <span className='text-lg font-medium'>Price Range</span>
+
+            <div className='flex flex-col gap-3'>
+                    <InputCategory name_input='prices' id_name="all" name="All"/>
+                    <InputCategory name_input='prices' id_name="under_200" name="Under $200"/>
+                    <InputCategory name_input='prices' id_name="200_500" name="$200 - $500"/>
+                    <InputCategory name_input='prices' id_name="500_1000" name="$500 - $1000"/>
+                    <InputCategory name_input='prices' id_name="over_1000" name="Over $1000"/>
+
+                    <div className='flex flex-col pt-3'>
+                        <div className='flex gap-16'>
+                            <span className='text-sm'>Min</span>
+                            <span className='text-sm'>Max</span>
+                        </div>
+
+                        <div className='flex gap-2'>
+                            <input type="number" className='w-16 border border-slate-300 rounded pl-3 py-1 text-sm' min={0}/>
+                            <span className='text-xl font-semibold'>-</span>
+                            <input type="number" className='w-16 border border-slate-300 rounded pl-3 py-1 text-sm' min={0}/>
+                        </div>
+                    </div>
+            </div>
+        </div>
+
+        <div className='pt-8 px-20 flex flex-col gap-4'>
+            <span className='text-lg font-medium'>Sort by</span>
+
+            <div className='flex flex-col gap-3'>
+                <select name="sort" className='w-fit px-2 py-2 text-sm font-light border border-slate-300 rounded'>
+                    <option value="newest" selected>Newest</option>
+                    <option value="low_to_high">Price: Low to High</option>
+                    <option value="high_to_low">Price: High to Low</option>
+                </select>
             </div>
         </div>
 
