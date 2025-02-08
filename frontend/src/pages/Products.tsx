409b70dffffa189b8ecd6605da7eaea5ab80b64b
davidvchang <dvalenzuelachang@gmail.com>
2025-02-08 01:20:15 -0700

    Change: background color of ProductsPage

--- frontend/src/pages/Products.tsx
+++ frontend/src/pages/Products.tsx
@@ -29,7 +29,7 @@ const Products:React.FC = () => {
     );
 
   return (
-    <section className='w-full h-full'>
+    <section className='w-full h-full bg-slate-50 pb-14'>
         <div className='w-full h-64 bg-slate-900 flex flex-col justify-center pl-20 gap-4'>
             <span className='text-white text-4xl font-light'>Products Collection</span>
             <span className='text-slate-300'>Discover our wide variety of high quality products.</span>
