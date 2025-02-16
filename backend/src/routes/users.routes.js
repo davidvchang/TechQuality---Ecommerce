fed75cf404b5cc3e921a273b4f7583801cb12e6b
davidvchang <dvalenzuelachang@gmail.com>
2025-02-15 20:23:25 -0700

    Add: login in routes.js

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,9 +1,11 @@
 import {Router} from "express"
-import {getAllUsers, registerUser} from '../controllers/users.controller.js'
+import {getAllUsers, registerUser, loginUser} from '../controllers/users.controller.js'
 
 const router = Router()
 
 router.get("/", getAllUsers)
 router.post("/", registerUser)
+
+router.post("/login", loginUser)
 
 export default router
