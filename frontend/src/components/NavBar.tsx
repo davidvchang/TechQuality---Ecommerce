d3fca573aa2cb90741225f4323b22efefb22eb1f
davidvchang <dvalenzuelachang@gmail.com>
2025-02-15 21:25:06 -0700

    Add: modal when is logged

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -7,6 +7,11 @@ interface PropsNavBar {
 }
 
 const NavBar:React.FC<PropsNavBar> = ({numberCart}) => {
+
+  const logout = () => {
+    
+  }
+
   return (
     <div className='h-16 w-full flex items-center justify-between px-5 bg-white border-b border-b-slate-200 shadow fixed z-10'>
         <h1 className='font-semibold text-2xl'>Tech<span className='text-blue-500'>Quality</span></h1>
@@ -19,7 +24,11 @@ const NavBar:React.FC<PropsNavBar> = ({numberCart}) => {
         </div>
 
         <div className='flex gap-5'>
-            <a href='/login' className='w-8 h-8 hover:text-blue-500 hover:transition duration-300 hover:cursor-pointer'><User/></a>
+            <a href='/login' className='relative w-8 h-8 hover:text-blue-500 hover:transition duration-300 hover:cursor-pointer'><User/></a>
+            <div className='flex flex-col absolute top-12 right-22 bg-white p-2 gap-5 shadow border border-slate-100 rounded'>
+              <span className='text-slate-600 font-light text-sm'>dvalenzuelachagn@gmail.com</span>
+              <button className='text-sm bg-red-500 py-2 text-white rounded hover:brightness-95 hover:transition duration-300 cursor-pointer'>Logout</button>
+            </div>
             <div className='flex'>
               <a href='/cart' className='w-7 h-8 hover:text-blue-500 hover:transition duration-300 hover:cursor-pointer'><ShoppingCart/></a>
               <div className=' bg-blue-500 px-1 py-0 w-fit h-fit rounded-full flex items-center select-none'>
