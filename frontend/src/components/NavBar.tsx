9be4339b07dcf0808fe80a8b4f1ad94ca49aafd8
davidvchang <dvalenzuelachang@gmail.com>
2025-02-18 21:45:01 -0700

    Change: usenavigate to windows.location.href

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -8,10 +8,12 @@ interface PropsNavBar {
 
 const NavBar:React.FC<PropsNavBar> = ({numberCart}) => {
 
+  
   const logout = () => {
-    
+    localStorage.removeItem("token");
+    window.location.href = "/login";
   }
-
+  
   return (
     <div className='h-16 w-full flex items-center justify-between px-5 bg-white border-b border-b-slate-200 shadow fixed z-10'>
         <h1 className='font-semibold text-2xl'>Tech<span className='text-blue-500'>Quality</span></h1>
@@ -25,10 +27,13 @@ const NavBar:React.FC<PropsNavBar> = ({numberCart}) => {
 
         <div className='flex gap-5'>
             <a href='/login' className='relative w-8 h-8 hover:text-blue-500 hover:transition duration-300 hover:cursor-pointer'><User/></a>
-            <div className='flex flex-col absolute top-12 right-22 bg-white p-2 gap-5 shadow border border-slate-100 rounded'>
-              <span className='text-slate-600 font-light text-sm'>dvalenzuelachagn@gmail.com</span>
-              <button className='text-sm bg-red-500 py-2 text-white rounded hover:brightness-95 hover:transition duration-300 cursor-pointer'>Logout</button>
-            </div>
+            {localStorage.getItem('token') !== null && (
+              <div className='flex flex-col absolute top-12 right-22 bg-white p-2 gap-5 shadow border border-slate-100 rounded'>
+                <span className='text-slate-600 font-light text-sm'>dvalenzuelachagn@gmail.com</span>
+                <button className='text-sm bg-red-500 py-2 text-white rounded hover:brightness-95 hover:transition duration-300 cursor-pointer' onClick={logout}>Logout</button>
+              </div>
+
+            )}
             <div className='flex'>
               <a href='/cart' className='w-7 h-8 hover:text-blue-500 hover:transition duration-300 hover:cursor-pointer'><ShoppingCart/></a>
               <div className=' bg-blue-500 px-1 py-0 w-fit h-fit rounded-full flex items-center select-none'>
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,19 +1,36 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const Login:React.FC = () => {
+
+  const URL_USERS: string = import.meta.env.VITE_URL_USERS
+
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const response = await axios.post(`${URL_USERS}/login`, {
+      email,
+      password,
+    });
+    localStorage.setItem('token', response.data.token);
+    // navigate('/');
+    window.location.href = "/";
+  };
   return (
     <section className='w-full bg-slate-50 p-10 flex flex-col items-center justify-center' style={{height: "calc(100vh - 64px)"}}>
 
-        <form className='bg-white border border-slate-200 p-10 rounded-lg shadow flex flex-col gap-5 w-80'>
+        <form className='bg-white border border-slate-200 p-10 rounded-lg shadow flex flex-col gap-5 w-80' onSubmit={handleLogin}>
         <span className='text-2xl text-center'>Login</span>
             <div className='flex flex-col gap-1'>
                 <label htmlFor="email" className='text-sm'>Email</label>
-                <input type="email"  placeholder='Email address' className='px-3 p-2 border border-slate-300 rounded-sm font-light text-sm' required/>
+                <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)}  placeholder='Email address' className='px-3 p-2 border border-slate-300 rounded-sm font-light text-sm' required/>
             </div>
 
             <div className='flex flex-col gap-1'>
                 <label htmlFor="password" className='text-sm'>Password</label>
-                <input type="password"  placeholder='Password' className='px-3 p-2 border border-slate-300 rounded-sm font-light text-sm' required/>
+                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' className='px-3 p-2 border border-slate-300 rounded-sm font-light text-sm' required/>
             </div>
 
             <button type="submit" className='bg-[#2B7FFF] text-white py-2 rounded hover:brightness-95 hover:transition duration-300 cursor-pointer'>Sign in</button>
