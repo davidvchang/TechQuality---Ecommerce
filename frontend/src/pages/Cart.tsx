41787a1b9b2dc04c3790f3ed45618479eab58a6b
davidvchang <dvalenzuelachang@gmail.com>
2025-02-20 00:11:26 -0700

    Add: show products in cart

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,9 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Minus, Plus, Trash2 } from 'lucide-react';
+import axios from 'axios';
+import Products from './Products';
+
+interface PropsDataCart {
+    id_cart_items: number,
+    user_id: number,
+    product_id: number,
+    quantity: number
+}
+
+interface PropsDataProducts {
+    id_product: number,
+    name: string,
+    description: string,
+    price: string,
+    category: string,
+    stock: number,
+    quantity: number;
+}
+
+interface PropsDataProductWithQuantity extends PropsDataProducts {
+    quantity: number;
+}
 
 const Cart:React.FC = () => {
 
+    const URL_GET_PRODUCTS_IN_CART = import.meta.env.VITE_URL_GET_CART_ITEMS
+    const URL_GET_PRODUCTS = import.meta.env.VITE_URL_PRODUCTS
+
     const [numberQuantity, setNumberQuantity] = useState<number>(1)
+    const [dataProductsUser, setDataProductsUser] = useState<PropsDataProducts[]>([])
 
     const restQuantity = () => {
         if(numberQuantity < 1) {
@@ -13,6 +40,42 @@ const Cart:React.FC = () => {
             setNumberQuantity(numberQuantity - 1)
         }
     }
+
+    const token = localStorage.getItem('token');
+
+    const getProducts = async () => {
+        const [response1, response2] = await Promise.all([
+            axios.get(`${URL_GET_PRODUCTS}/`),
+            axios.get(`${URL_GET_PRODUCTS_IN_CART}/`, 
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            )
+        ]);
+
+        const allProducts:Array<PropsDataProducts> = response1.data;
+        const cartItems:Array<PropsDataCart> = response2.data;
+
+        //SAVE JUST PRODUCTS FROM USER AUTHENTICATED
+        const productsUser: PropsDataProductWithQuantity[] = allProducts.filter(product => 
+            {
+                const cartItem = cartItems.find(item => item.product_id === product.id_product)
+                if(cartItem) {
+                    product.quantity = cartItem.quantity;
+                    return true;
+                }
+                return false
+            })
+        
+        setDataProductsUser(productsUser)
+    }
+
+    useEffect(() => {
+        getProducts()
+    }, [])
+    
 
   return (  
     <section className='flex flex-col p-10 gap-5 bg-slate-50'>
@@ -24,36 +87,39 @@ const Cart:React.FC = () => {
             <div className='w-full flex flex-col justify-between gap-1'>
 
                 {/* CONTAINER PRODUCT */}
-                <div className='flex justify-between bg-white p-5 rounded-lg border w-full h-fit border-slate-100'>
+                {dataProductsUser.map((product) => (
 
-                    <div className='flex items-center gap-5'>
-                        <div className='bg-red-400 min-w-24 min-h-24 max-h-24 overflow-hidden rounded'>
-                            <img src="" alt="" className='w-full h-full'/>
+                    <div className='flex justify-between bg-white p-5 rounded-lg border w-full h-fit border-slate-100' key={product.id_product}>
+
+                        <div className='flex items-center gap-5'>
+                            <div className='bg-red-400 min-w-24 min-h-24 max-h-24 overflow-hidden rounded'>
+                                <img src="" alt="" className='w-full h-full'/>
+                            </div>
+
+                            <div className='flex flex-col gap-4'>
+                                <div className='flex flex-col gap-1'>
+                                    <span className='font-medium text-lg'>{product.name}</span>
+                                    <span className='text-sm text-gray-500 font-normal'>{product.category}</span>
+                                    <span className='text-sm text-gray-500 font-normal'>Unit price: ${product.price}</span>
+                                </div>
+
+                                <div className='flex border w-fit border-slate-200 rounded gap-2 px-1 items-center'>
+                                    <button className='w-fit h-fit p-2 hover:bg-slate-50 cursor-pointer hover:transition duration-300' onClick={restQuantity}><Minus className='w-4 h-4'/></button>
+                                    <span className='py-2 px-1'>{product.quantity}</span>
+                                    <button className='w-fit h-fit p-2 hover:bg-slate-50 cursor-pointer hover:transition duration-300' onClick={() => setNumberQuantity(numberQuantity + 1)}><Plus className='w-4 h-4'/></button>
+                                </div>
+                            </div>
+
                         </div>
 
-                        <div className='flex flex-col gap-4'>
-                            <div className='flex flex-col gap-1'>
-                                <span className='font-medium text-lg'>Alexa</span>
-                                <span className='text-sm text-gray-500 font-normal'>Technology</span>
-                                <span className='text-sm text-gray-500 font-normal'>Unit price: $799.99</span>
-                            </div>
+                        <div className='flex flex-col items-end min-h-full justify-between'>
+                            <span>${Number(product.price) * numberQuantity}</span>
+                            <button className='w-fit p-2 rounded cursor-pointer hover:text-red-500 hover:bg-red-100 hover:transition duration-300'><Trash2 className='w-5 h-5'/></button>
 
-                            <div className='flex border w-fit border-slate-200 rounded gap-2 px-1 items-center'>
-                                <button className='w-fit h-fit p-2 hover:bg-slate-50 cursor-pointer hover:transition duration-300' onClick={restQuantity}><Minus className='w-4 h-4'/></button>
-                                <span className='py-2 px-1'>{numberQuantity}</span>
-                                <button className='w-fit h-fit p-2 hover:bg-slate-50 cursor-pointer hover:transition duration-300' onClick={() => setNumberQuantity(numberQuantity + 1)}><Plus className='w-4 h-4'/></button>
-                            </div>
                         </div>
 
                     </div>
-
-                    <div className='flex flex-col items-end min-h-full justify-between'>
-                        <span>$799.99</span>
-                        <button className='w-fit p-2 rounded cursor-pointer hover:text-red-500 hover:bg-red-100 hover:transition duration-300'><Trash2 className='w-5 h-5'/></button>
-
-                    </div>
-
-                </div>
+                ))}
 
             </div>
 
